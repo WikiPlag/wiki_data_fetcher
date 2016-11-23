f1eb9858a47c4de4d6f42fef5fbcd6618e54350e
Timothe Genzmer <timothegenzmer@gmail.com>
2016-11-23 22:11:19 +0100

    Make WikiCollection serializable

--- src/main/scala/de/htw/ai/wikiplag/spark/WikiCollection.scala
+++ src/main/scala/de/htw/ai/wikiplag/spark/WikiCollection.scala
@@ -4,7 +4,8 @@ import com.mongodb.{CursorType, ServerAddress}
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.{MongoClient, MongoCollection}
 
-class WikiCollection(createWikiCollection: () => MongoCollection) {
+class WikiCollection(createWikiCollection: () => MongoCollection)
+  extends Serializable {
   lazy val wikiCollection = createWikiCollection()
 
   def insertArticle(wikiID: Long,
@@ -26,17 +27,17 @@ class WikiCollection(createWikiCollection: () => MongoCollection) {
 }
 
 object WikiCollection {
-  val WikiCollectionName = "documents"
-
   def apply(mongoDBPath: String, port: Int, mongoDBUser: String, mongoDBPW: String, database: String): WikiCollection = {
+    val WikiCollectionName = "documents"
     //http://stackoverflow.com/questions/25825058/why-multiple-mongodb-connecions-with-casbah
     val createWikiCollectionFct = () => {
-
       val mongoClient = MongoClient(
         new ServerAddress(mongoDBPath, port),
         List(MongoCredential.createCredential(mongoDBUser, mongoDBUser, mongoDBPW.toCharArray))
       )
-
+      sys.addShutdownHook {
+        mongoClient.close()
+      }
       mongoClient(database)(WikiCollectionName)
 
     }
